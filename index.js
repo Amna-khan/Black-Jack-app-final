82c796ed282e89517bbbf633b3eed55658c92c1c
Amna-khan <32608171+Amna-khan@users.noreply.github.com>
2021-10-01 06:10:43 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,67 @@
-let firstCard = 10;
-let secondCard = 11;
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
+let cards = [firstCard, secondCard];
+let sum = firstCard + secondCard ;
 let hasBlackJack = false;
 let isAlive = true
 let message = "";
-let sum = firstCard + secondCard;
 
-if (sum < 21) {
-    message = "Do you want to draw a new card? 🙂 ";
 
-} else if(sum == 21) {
-    message = "You have got black jack! 🥳 ";
-    hasBlackJack = false;
-} else {
-    message = "You are out of the game! 😭 ";
-    isAlive = false;
+
+function getRandomCard(){
+
+
 }
 
-console.log(message);
+
+
+
+let messageEl = document.getElementById("message-el");
+console.log(messageEl);
+
+let sumEl = document.querySelector(".sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+function renderGame() {
+    cardsEl.textContent = "Cards: " ;
+
+    for (let i = 0; i < cards.length; i++){     //for loop
+        cardsEl.textContent += cards[i] + " "; //print cards with spaces
+    }
+
+
+    sumEl.textContent = "Sum: " + sum;
+    if (sum < 20) {
+        message = "Do you want to draw a new card?";
+    
+    } else if(sum === 21) {
+        message = "You have got black jack!";
+        hasBlackJack = false;
+    } else {
+        message = "You are out of the game!";
+        isAlive = false;
+    }
+
+    messageEl.textContent = message;
+    
+}
+
+function startGame() {
+    renderGame();
+}
+
+
+function newCard() {
+    console.log("new Card!")
+    
+    let newCard = getRandomCard();
+
+    sum += newCard;
+
+    cards.push(newCard);
+
+    renderGame();
+}
+
+
+
