2155fad3c62463b14f82a992759a36cd84fb00d4
Amna-khan <32608171+Amna-khan@users.noreply.github.com>
2021-10-07 06:10:19 -0700

    Completed

--- index.js
+++ index.js
@@ -1,31 +1,47 @@
-let firstCard = getRandomCard();
-let secondCard = getRandomCard();
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard ;
+
+let cards = [];
+let sum = 0 ;
 let hasBlackJack = false;
-let isAlive = true
+let isAlive = false;
 let message = "";
 
-
-
-function getRandomCard(){
-    let num = Math.floor(Math.random() * 13) + 1
-    
-    if (num == 1){
-        return 11
-    } else if (num > 11 && num <13) {
-        return 10
-    }
+let player = { //Object Creation
+        name : "Per",
+        chips : 145
 }
 
-
-
+let playerEl = document.getElementById("player-el");
+playerEl.textContent = player.name + ": $"+ Player.chips;
 
 let messageEl = document.getElementById("message-el");
 console.log(messageEl);
 
 let sumEl = document.querySelector(".sum-el")
 let cardsEl = document.getElementById("cards-el")
+
+function startGame() {
+    isAlive = true;
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards = [firstCard, secondCard];
+    sum = firstCard + secondCard;
+
+    renderGame();
+}
+
+
+function getRandomCard(){
+    let num = Math.floor(Math.random() * 13) + 1
+    
+    if (num > 10){
+        return 10
+    } else if (num === 1 ) {
+        return 11
+    } else {
+        return num
+    }
+}
+
 
 function renderGame() {
     cardsEl.textContent = "Cards: " ;
@@ -51,12 +67,11 @@ function renderGame() {
     
 }
 
-function startGame() {
-    renderGame();
-}
-
 
 function newCard() {
+
+    if (isAlive == true && hasBlackJack == false) {
+
     console.log("new Card!")
     
     let newCard = getRandomCard();
@@ -66,6 +81,7 @@ function newCard() {
     cards.push(newCard);
 
     renderGame();
+    }
 }
 
 
